2b525f7743dc638d4da0cc7caa9d460a8f468ec7
nhatpro586 <62631142+nhatpro586@users.noreply.github.com>
2020-04-03 22:59:12 +0700

    unfinished bt34

--- examples.js
+++ examples.js
@@ -1,20 +1,37 @@
-let mStr= "welcome To the realm";
+let mStr= "Coronavirus disease (COVID-19) is an infectious disease caused by a new virus. The disease causes respiratory illness (like the flu) with symptoms such as a cough, fever, and in more severe cases, difficulty breathing. You can protect yourself by washing your hands frequently, avoiding touching your face, and avoiding close contact (1 meter or 3 feet) with people who are unwell.";
 
-function lowStr(str) {
-	return str.toLowerCase();
+function insertStr(bStr,str,pos) {
+	let rs="";
+	rs = bStr.slice(0,pos) + str + bStr.slice(pos);
+	return rs;
 }
 
-function capitalStr(str) {
-	str = str.split(" ");
-	
-	let strLen = str.length;
-
-	for (let i = 0; i < strLen; i++) {
-		str[i] = str[i][0].toUpperCase() + str[i].substr(1);
+function splitStr(str) {
+	let count=0;
+	for (let i=0;i<str.length;i++) {
+		if (str[i]=="\n") count =0;
+		if (str[i]==" ") count+=1;
+		if (count==6) {
+			str=insertStr(str, "\n", i+1);
+			count =0;
+		}
 	}
-
-	return str.join(" ");
+	return str;
 }
 
-console.log(lowStr(mStr));
-console.log(capitalStr(mStr));
+function countSpc(str) {
+	let count=0;
+	let line=1;
+	for (let i=0;i<str.length;i++) {
+		if (str[i]==" ") count+=1;
+		if (str[i]==" \n") {
+			console.log("Dong",line,"co", count, "khoang trong");
+			line+=1;
+			count =0;
+		}
+	}
+	return "Done!!";
+}
+
+console.log(splitStr(mStr));
+console.log(countSpc(mStr));
